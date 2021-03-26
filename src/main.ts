83526115605eab6003ab916dc172cb20d8963191
lucas <lucas.flaquer@outlook.com>
2021-03-26 08:10:53 -0300

    babylon main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import "@babylonjs/core/Debug/debugLayer";
-import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
-import { ArcRotateCamera, Engine, HemisphericLight, MeshBuilder, Scene, SceneLoader, Vector3 } from "@babylonjs/core"
+import { ArcRotateCamera, Color3, Engine, HemisphericLight, MeshBuilder, Scene, Sound, StandardMaterial, Texture, Vector3, Vector4 } from "@babylonjs/core"
+import { notes, blackKeys, whiteKeys } from "./component/notes";
+import { animeteKeyPress } from "./functions/animateKeyPress";
 
 const canvasElement = document.createElement('canvas')
 canvasElement.id = "renderCanvas"
@@ -11,7 +12,66 @@ const createScene = (engine: Engine, canvas: HTMLCanvasElement) => {
   const camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new Vector3(0, 0, 0), scene);
   camera.attachControl(canvas, true);
   const light = new HemisphericLight("light", new Vector3(1, 1, 0), scene);
-  SceneLoader.ImportMeshAsync("semi_house", "https://assets.babylonjs.com/meshes/", "both_houses_scene.babylon");
+
+  const groundMat = new StandardMaterial("groundmaterial", scene);
+  groundMat.diffuseColor = new Color3(0, 0, 0)
+  const ground = MeshBuilder.CreateGround("ground", { width: 10, height: 5 })
+  ground.material = groundMat
+  ground.isPickable = false
+
+  const box1Material = new StandardMaterial("box1material", scene)
+  box1Material.diffuseTexture = new Texture("./assets/teclado.jpg", scene)
+  const faceUV = [];
+  faceUV[0] = new Vector4(0 * 1 / 4, 1 * 1 / 4, 1 * 1 / 4, 2 * 1 / 4); //rear face
+
+  const box1 = MeshBuilder.CreateBox("box1", { height: .5, width: 10, depth: 3 }, scene)
+  box1.position.y = 0.25
+  box1.position.z = 1
+  box1.material = box1Material
+
+
+
+  const whiteKeyMaterial = new StandardMaterial('color', scene)
+  whiteKeyMaterial.diffuseColor = new Color3(255, 255, 255)
+  const blackKeyMaterial = new StandardMaterial('color', scene)
+  blackKeyMaterial.diffuseColor = new Color3(0, 0, 0)
+  const initialPos = -4
+  whiteKeys.forEach((note, index) => {
+    const WhiteKey = MeshBuilder.CreateBox(note.name, { height: .2, width: .5, depth: 2 }, scene)
+    WhiteKey.material = whiteKeyMaterial
+    WhiteKey.position.y = 0.2;
+    WhiteKey.position.z = -1.35;
+    WhiteKey.position.x = initialPos + 0.55 * index
+  })
+
+  let blackPosition = initialPos
+  blackKeys.forEach((note, index) => {
+    const BlackKey = MeshBuilder.CreateBox(note.name, { height: .3, width: 3 / 5 * 0.5, depth: 1.4 }, scene)
+    if (index === 2 || index === 7)
+      blackPosition += .5
+    if (index === 5)
+      blackPosition += .6
+
+    BlackKey.material = blackKeyMaterial
+    BlackKey.position.y = 0.2;
+    BlackKey.position.z = -0.9;
+    BlackKey.position.x = blackPosition + .3 + 0.55 * index
+  })
+
+  scene.onPointerDown = (evt, pickResults) => {
+    if (pickResults.hit) {
+      if (pickResults) {
+        const note = notes.find(note => note.name === pickResults.pickedMesh?.name);
+        if (note) {
+          const sound = new Sound(note.name, note.sound, scene, null, { loop: false, autoplay: true })
+          sound.setVolume(5)
+          scene.beginDirectAnimation(pickResults.pickedMesh, [animeteKeyPress(scene, note.name)], 0, 100, false)
+        }
+      }
+
+    }
+  }
+
 
   return scene;
 }
@@ -32,3 +92,4 @@ rootEl.appendChild(canvasElement)
 window.addEventListener("resize", () => {
   engine.resize()
 })
+engine.resize()
